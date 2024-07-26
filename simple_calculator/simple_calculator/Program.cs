0df362c2a7b3a994dc02384c0b613849b1804762
lazzerex <150422206+lazzerex@users.noreply.github.com>
2024-07-26 17:07:23 +0700

    Add files via upload

---
+++ simple_calculator/simple_calculator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value;
+            do
+            {
+                int result;
+                Console.Write("Enter first number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter second number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter symbol(/,+,-,*):");
+                string symbol = Console.ReadLine();
+
+                switch (symbol)
+                {
+                    case "+":
+                        result = num1 + num2;
+                        Console.WriteLine("Addition: " + result);
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        Console.WriteLine("Substraction: " + result);
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        Console.WriteLine("Substraction: " + result);
+                        break;
+                    case "/":
+                        result = num1 / num2;
+                        Console.WriteLine("Substraction: " + result);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong inputs");
+                        break;
+                }
+
+                Console.WriteLine(" ");
+                Console.Write("Do you want to continue (y/n): ");
+                value = Console.ReadLine();
+
+            }
+            while (value == "y" || value == "Y");
+        }
+    }
+}
+
